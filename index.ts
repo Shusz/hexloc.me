a8047e30377fc46a0285537be8f709dece151029
hexlocation <hexlocation@skiff.com>
2023-10-28 18:32:57 +0000

    more cleaning up!

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 const server = Bun.serve({
   port: 3000,
   fetch(request) {
-    console.log(import.meta.dir + "//icons//loading.gif");
     const path = new URL(request.url).pathname;
-    console.log(path);
     switch (path) {
       case "/":
         return new Response(Bun.file(import.meta.dir + "//index.html"));
@@ -11,18 +9,10 @@ const server = Bun.serve({
       case "/cat":
         return new Response(Bun.file(import.meta.dir + "//cat.html"));
         break;
-      case "/_ico/loading":
-        return new Response(Bun.file(import.meta.dir + "//icons//loading.gif"));
-        break;
     }
     if (path.startsWith("/md/")) {
       return new Response(
         Bun.file(import.meta.dir + "//md//" + path.replace("/md/", ""))
-      );
-    }
-    if (path.startsWith("/isos/")) {
-      return new Response(
-        Bun.file(import.meta.dir + "//isos//" + path.replace("/isos/", ""))
       );
     }
   },
